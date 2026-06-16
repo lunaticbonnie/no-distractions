fdf6450acd3e418ba0b4c8ff2472a57ecc46c059
lunaticbonnie <lunaticbonnie@gmail.com>
2026-06-16 13:54:21 +0200

    fix for fabric-1.21.6

---
+++ mod_overrides/src/main/java/patrolin/nodistractions/mixin/ServerPlayerMixin-1.21.6.java
@@ -0,0 +1,45 @@
+package patrolin.nodistractions.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.Consumer;
+
+@Mixin(ServerPlayer.class)
+public class ServerPlayerMixin {
+  // night rework
+  @WrapOperation(method="startSleepInBed", at=@At(value="INVOKE", target="Lnet/minecraft/server/level/ServerPlayer;setRespawnPosition(Lnet/minecraft/server/level/ServerPlayer$RespawnConfig;Z)V"))
+  private void setRespawnPoint(ServerPlayer instance, ServerPlayer.RespawnConfig respawnConfig, boolean bl, Operation<Void> original) {}
+  @WrapOperation(method="startSleepInBed", at= @At(value="INVOKE", target="Lnet/minecraft/server/level/ServerLevel;isBrightOutside()Z"))
+  private boolean isDaytime(ServerLevel instance, Operation<Boolean> original, @Local(argsOnly = true) BlockPos blockPos) {
+    boolean result = original.call(instance);
+    if (result) setRespawnPosition(blockPos);
+    return result;
+  }
+  @WrapOperation(method="startSleepInBed", at=@At(value="INVOKE", target="Lcom/mojang/datafixers/util/Either;ifRight(Ljava/util/function/Consumer;)Lcom/mojang/datafixers/util/Either;"))
+  private <L,R>Either<L, R> onSleep(Either<L,R> instance, Consumer<? super R> consumer, Operation<Either<L, R>> original, @Local(argsOnly = true) BlockPos blockPos) {
+    return instance.ifRight((unit) -> {
+      setRespawnPosition(blockPos);
+      consumer.accept(unit);
+    });
+  }
+
+  @Unique
+  private void setRespawnPosition(BlockPos blockPos) {
+    ServerPlayer player = (ServerPlayer)(Object)this;
+    player.setRespawnPosition(new ServerPlayer.RespawnConfig(player.level().dimension(), blockPos, player.getYRot(), false), true);
+  }
+
+  // hunger rework
+  @WrapMethod(method="tickRegeneration")
+  private void peacefulRegeneration(Operation<Void> original) {}
+}
